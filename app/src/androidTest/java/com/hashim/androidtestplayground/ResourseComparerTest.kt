4b285ece88f866c29db2b7aa088dcd51f5123a4a
hashim <hashimtahir@live.com>
2020-08-21 10:45:57 +0500

    Android first instrumentation test added.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/androidTest/java/com/hashim/androidtestplayground/ResourseComparerTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020/  8/ 21.  Created by Hashim Tahir
+ */
+
+package com.hashim.androidtestplayground
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth;
+import org.junit.Before
+import org.junit.Test
+
+class ResourseComparerTest {
+    private lateinit var hResourseComparer: ResourseComparer
+
+    /*Setup on anything added in before is executed before every test*/
+    @Before
+    fun hSetup() {
+        hResourseComparer = ResourseComparer()
+
+    }
+
+    /*Get context to get the resourses*/
+    @Test
+    fun hStringResourseSameAsGivenString_retrunsTrue() {
+        var applicationContext = ApplicationProvider.getApplicationContext<Context>()
+
+        val hResult = hResourseComparer.hIsequal(
+            applicationContext,
+            R.string.app_name,
+            "Android Test Playground"
+        )
+        Truth.assertThat(hResult).isTrue()
+    }
+
+    @Test
+    fun hStringResourseNotSameAsGivenString_retrunsFalse() {
+        var applicationContext = ApplicationProvider.getApplicationContext<Context>()
+        val hResult = hResourseComparer.hIsequal(
+            applicationContext,
+            R.string.app_name,
+            "Hashim"
+        )
+        Truth.assertThat(hResult).isFalse()
+    }
+}
---
+++ app/src/main/java/com/hashim/androidtestplayground/ResourseComparer.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020/  8/ 21.  Created by Hashim Tahir
+ */
+
+package com.hashim.androidtestplayground
+
+import android.content.Context
+
+class ResourseComparer {
+    fun hIsequal(context: Context, restId: Int, string: String): Boolean {
+        return context.getString(restId).equals(string)
+    }
+}
